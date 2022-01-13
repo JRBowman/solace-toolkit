bbd2b0ab2c1f038cebc8a286db39724408e13fec
Justin Bowman <justin@onbowman.com>
2022-01-13 12:48:50 -0500

    major overhaul for markdown support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { MsoaUserService } from './services/msoa-user-service';
+import { Component } from '@angular/core';
+import { MarkdownService } from 'ngx-markdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(public oidcSecurityService: OidcSecurityService, public msoaUserService: MsoaUserService) {}
-  title = 'angular-ui';
+export class AppComponent {
 
-  ngOnInit(){
-    // Process Authorization checks here:
-    // this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData, accessToken, idToken}) => {
-    //   this.msoaUserService.SetState(accessToken, isAuthenticated, idToken, userData)
-    // })
-  }
-
-  login(){
-    this.msoaUserService.Login();
-  }
-
-  logout(){
-    this.msoaUserService.Logout();
-  }
+  constructor(private markdownService: MarkdownService) { }
+  title = 'onbowman-13';
 }
