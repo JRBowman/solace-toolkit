1b63a99285b148842d016d09b9e9d33e4b907ea5
Justin Bowman <justin@onbowman.com>
2022-09-04 19:01:49 -0400

    fix authn / authz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { MarkdownService } from 'ngx-markdown';
+import { MsoaUserService } from './services/msoa-user-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private markdownService: MarkdownService) {
+  constructor(private markdownService: MarkdownService, public oidcSecurityService: OidcSecurityService, public msoaUserService: MsoaUserService) {
     setInterval(() => {
       this.time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'});
     }, 1);
    }
+
+
+  ngOnInit(): void {
+        // Process Authorization checks here:
+        this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData, accessToken, idToken}) => {
+          this.msoaUserService.SetState(accessToken, isAuthenticated, idToken, userData)
+        })
+  }
+
+
   title = 'Solace Toolkit (TK) for Game Development';
   time = new Date().toLocaleTimeString();
+
 }
