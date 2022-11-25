96d617f1e1c730fcbeedbaa078c19e4cded47ca0
Justin Bowman <justin@onbowman.com>
2022-11-24 23:30:21 -0500

    updates

--- src/app/solace-toolkit/controllers-components/enemy-controllers/enemy-controllers.component.ts
+++ src/app/solace-toolkit/controllers-components/enemy-controllers/enemy-controllers.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { SolacetkSearchSheetComponent } from '../../common/solacetk-search-sheet/solacetk-search-sheet.component';
 import { BehaviorComponent } from '../../models/behavioranimation';
+import { BehaviorBranch } from '../../models/behaviorbranch';
+import { BehaviorState } from '../../models/behaviorstate';
 import { CollisionDetectionType, MovableController, MovableControllerType } from '../../models/movablecontroller';
+import { SoltkKeyValue } from '../../models/soltk-key-value';
 
 @Component({
   selector: 'app-enemy-controllers',
@@ -24,9 +27,36 @@ export class EnemyControllersComponent implements OnInit {
   public collidableType = CollisionDetectionType;
   public collidableTypes: string[] = [];
 
+
+  public testStateData: SoltkKeyValue[] = [];
+  public testStateDataChange = new EventEmitter<SoltkKeyValue[]>();
+  public testSelectedState?: BehaviorState;
+
+
+  private _selectedBranch?: BehaviorBranch;
   ngOnInit(): void {
     this.movableTypes = Object.keys(this.moveType).filter(f => !isNaN(Number(f)));
     this.collidableTypes = Object.keys(this.collidableType).filter(f => !isNaN(Number(f)));
+
+    this.testStateDataChange.subscribe(value => {
+      console.log(value);
+    });
+  }
+
+  public selectState()
+  {
+    // If a State is Selected - wait till it's conditions no longer apply:
+    if (this.testSelectedState && this.validateConditions(this.testSelectedState.conditions)) return; 
+
+    // No State, find the first valid Branch:
+    if (!(this._selectedBranch = this.model.behaviorSystem?.branches.find(x => this.validateConditions(x.conditions)))) return;
+
+    if (!(this.testSelectedState = this._selectedBranch.states.find(x => this.validateConditions(x.conditions)))) return;
+  }
+
+  public validateConditions(conditions: SoltkKeyValue[]): boolean
+  {
+    return true;
   }
 
   public GetMoveType(val: string): string
