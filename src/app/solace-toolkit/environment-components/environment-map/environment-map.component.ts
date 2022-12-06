fbee65cc901672ce5db2caebe072dd0e0a8a3fca
Justin Bowman <justin@onbowman.com>
2022-12-06 01:28:35 -0500

    updates to map viewer

--- src/app/solace-toolkit/environment-components/environment-map/environment-map.component.ts
+++ src/app/solace-toolkit/environment-components/environment-map/environment-map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragStart } from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
 import { EnvironmentMap } from '../../models/environment-map';
 import { EnvironmentMapLayer } from '../../models/environment-map-layer';
 import { SolacetkService } from '../../services/solacetk-service.service';
@@ -16,15 +18,27 @@ export class EnvironmentMapComponent implements OnInit {
 
   public selectedLayer: EnvironmentMapLayer = new EnvironmentMapLayer();
 
+  public mapscale: any = 2;
+  public mapscaleChange = new EventEmitter<number>();
+
+  public scaleXY = 'scale(' + this.mapscale + ',' + this.mapscale + ')';
+
 
   public profileUrl: string = "";
 
   ngOnInit(): void {
+
   }
 
   loadEditor()
   {
     this.profileUrl = this.soltkService.apiHost + "Ase/" + this.model.name + "-act/" + this.model.name + "-act.png";
+  }
+
+  changeScale(event: any) {
+    this.scaleXY = 'scale(' + this.mapscale + ',' + this.mapscale + ')';
+    console.log("CHNAGE");
+    console.log(this.mapscale);
   }
 
   public logModel()
