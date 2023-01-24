2a25530dcc3784eb179dedec9c9271bcd55d99f7
Justin Bowman <justin@onbowman.com>
2023-01-23 23:33:33 -0500

    update prod target uRL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: "https://msoa-arranger-bowman-prod.apps.bocp.onbowman.com/",
+  apiHost: "https://solacetk-core-bowman-dev.apps.bocp.onbowman.com/",
   identityHost: "https://identity-svc.onbowman.com"
 };
