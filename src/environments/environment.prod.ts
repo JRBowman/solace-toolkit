91dd6d36254e330b7f59d6e73d7c079165270ad9
Justin Bowman <justin@onbowman.com>
2023-02-13 21:33:45 -0500

    updated prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: "https://solacetk-core-bowman-dev.apps.bocp-rosa.hpw3.p1.openshiftapps.com/",
+  apiHost: "https://solacetk-core-bowman-dev.apps.bocp.onbowman.com/",
   identityHost: "https://identity-svc.onbowman.com"
 };
