564d3dafde002f02a73cf6771c4afc6458134b13
Justin R Bowman <justin@onbowman.com>
2023-04-18 16:17:11 +0000

    environment.prod.ts edited online with Bitbucket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: "https://solacetk-core-bowman-dev.apps.bocp.onbowman.com/",
+  apiHost: "https://solacetk-core-bow-dev.apps.cluster-n4gnv.n4gnv.sandbox1875.opentlc.com/",
   identityHost: "https://identity-svc.onbowman.com"
 };
